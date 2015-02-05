a73f73d144a0f9abc6093468fcbc9bc7cdccf4fb
Marius Flage <marius@ksat.no>
2015-02-05 15:45:11 +0100

    C code that will run on the Pebble watch itself. Initial commit.

---
+++ main.c
@@ -0,0 +1,107 @@
+#include <pebble.h>
+
+#define KEY_PLAYER1 0
+#define KEY_PLAYER2 1
+
+static Window *s_main_window;
+static TextLayer *s_time_layer;
+
+/*
+static int *player1;
+static int *player2;
+static char *player1name[30];
+static char *player2name[30];
+*/
+
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+  // Store incoming information
+  static char player1_buffer[30];
+  static char player2_buffer[30];
+  static char match_info_buffer[30];
+    
+  // Read first item
+  Tuple *t = dict_read_first(iterator);
+
+  // For all items
+  while(t != NULL) {
+    // Which key was received?
+    switch(t->key) {
+    case KEY_PLAYER1:
+      snprintf(player1_buffer, sizeof(player1_buffer), "%s", t->value->cstring);
+      break;
+    case KEY_PLAYER2:
+      snprintf(player2_buffer, sizeof(player2_buffer), "%s", t->value->cstring);
+      break;
+    default:
+      APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognized!", (int)t->key);
+      break;
+    }
+    // Look for next item
+    t = dict_read_next(iterator);
+  } // end of while loop
+
+  // build the buffer
+  snprintf(match_info_buffer, sizeof(match_info_buffer), "%s vs %s", player1_buffer, player2_buffer);
+  text_layer_set_text(s_time_layer, match_info_buffer);
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+
+static void main_window_load(Window *window) {
+  // Create time TextLayer
+  s_time_layer = text_layer_create(GRect(5, 52, 139, 50));
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorBlack);
+  
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+}
+
+static void main_window_unload(Window *window) {
+  // Destroy TextLayer
+  text_layer_destroy(s_time_layer);
+}
+
+/* pretty basic init function */
+static void init() {
+  s_main_window = window_create();
+
+  // Set handlers to manage the elements inside the Window
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
+
+  // Show the Window on the watch, with animated=true
+  window_stack_push(s_main_window, true);
+  
+  // Register callbacks
+  app_message_register_inbox_received(inbox_received_callback);
+  app_message_register_inbox_dropped(inbox_dropped_callback);
+  app_message_register_outbox_failed(outbox_failed_callback);
+  app_message_register_outbox_sent(outbox_sent_callback);
+  
+  // Open AppMessage
+  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+}
+
+static void deinit() {
+  // Destroy Window
+  window_destroy(s_main_window);
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+}
